81ae37b666f11e9923a7e58dfb80cf907df4e441
libin1412 <84071414+libin1412@users.noreply.github.com>
2024-07-23 10:08:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Our 1st Streamlit App")
+st.subheader("Introducing Streamlit in Automate Everything with Python")
+st.write("""This is our first web app.
+Enjoy it!""")
+
+
+
+
+
+
+
+
+  
+
+    
+  
---
+++ replit.nix
@@ -0,0 +1,19 @@
+{pkgs}: {
+  deps = [
+    pkgs.streamlit
+    pkgs.jre
+    pkgs.xcbuild
+    pkgs.swig
+    pkgs.openjpeg
+    pkgs.mupdf
+    pkgs.libjpeg_turbo
+    pkgs.jbig2dec
+    pkgs.harfbuzz
+    pkgs.gumbo
+    pkgs.freetype
+    pkgs.libiconv
+    pkgs.geckodriver
+    pkgs.chromium
+    pkgs.chromedriver
+  ];
+}
