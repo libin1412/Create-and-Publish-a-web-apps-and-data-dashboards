ea3e9cd06cc53dfc938825447d6a0dc90d0b16b2
libin babu <8704447-libinbabu@users.noreply.replit.com>
2024-07-23 10:30:19 +0000

    app update 1

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import streamlit as st
+import pandas 
+
+data = {
+  'Series_1':[1,3,4,5,7],
+  'Series_2':[10,30,40,100,250]
+}
+
+df = pandas.DataFrame(data)
 
 st.title("Our 1st Streamlit App")
 st.subheader("Introducing Streamlit in Automate Everything with Python")
 st.write("""This is our first web app.
 Enjoy it!""")
+
+st.write(df)
 
 
 
